7d2d77fce67c59751bd42d47d0be27235da3c316
msobocin <msobocin@192.168.120.37>
2015-02-10 20:14:01 +0100

    Subir

---
+++ Controler/BD.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author Michal Sobocisnk
+ */
+
+class BD {
+	protected  $_link;
+	
+	public function __construct(){
+// 		$this->_connectBD();
+	}
+	
+	public function connectBD() {
+		try{
+			$this->_link = new PDO("mysql:host=localhost;dbname=receptasmcg", "root", "secret");
+		
+			$this->_link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			
+		}catch (PDOException $e){
+			throw  $e;
+		}
+		
+	}
+	
+	public function disconnectBD() {
+		//mysqli::close();
+		try {
+			$this->_link = null;
+		} catch (Exception $e) {
+			throw $e;
+		}
+		
+	}
+	
+// 	public function __getLink(){
+// 		return $this->_link;
+// 	}
+}
+
+?>
