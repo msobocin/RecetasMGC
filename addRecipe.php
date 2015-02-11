e0b2457341835a58840b633c722bad10eb73e0df
msobocin <msobocin@jaumebalmes.net>
2015-02-11 12:29:21 +0100

    Update addRecipe.php

--- addRecipe.php
+++ addRecipe.php
@@ -1,6 +1,7 @@
 <?php 
 //aqui va folmulario para añadir nuevas receptas :D
 //con comprobaciones en lado php
+//tambien el folmulario va tener un campo con js donde va ser selectbox o algo si de los ingredientes, para crear una array de los ingredientes que pasamos a recepta
 
 
 ?>
