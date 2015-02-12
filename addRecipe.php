f357117ef5a32d946cca8d774728adf67f57ef3a
Michal Sobocinski <msobocin@jaumebalmes.net>
2015-02-12 23:06:38 +0100

    comentarios

--- addRecipe.php
+++ addRecipe.php
@@ -1,7 +1,5 @@
 <?php 
 //aqui va folmulario para añadir nuevas receptas :D
-//con comprobaciones en lado php
-//tambien el folmulario va tener un campo con js donde va ser selectbox o algo si de los ingredientes, para crear una array de los ingredientes que pasamos a recepta
 
 
 ?>
