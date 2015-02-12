85d0619dbd61df45a9d2b1335dbb000b11b5c419
GraceToa <gracetoa29@gmail.com>
2015-02-12 17:24:06 +0100

    View

--- View/RecetaView.php
+++ View/RecetaView.php
@@ -1,6 +1,40 @@
 <?php
-require_once 'Model/Receta.php';
+require_once 'model/Book.php';
 
-class RecetaView {
+require_once 'model/Author.php';
+require_once 'controler/AuthorControler.php';
 
+class BookView {
+
+	public function view($recetas) {
+		?>
+		<table class="table table-hover">
+			<tr>
+					<th>Nombre</th>
+					<th>Ingredientes</th>
+					<th>Preparacion</th>
+					<th>Tiempo</th>
+			</tr>
+			
+		  <?php
+		  		foreach ($recetas as $receta) {
+	
+					echo "<tr>";					
+					echo "<td>" . $receta->getNombre() . "</td>";
+					echo "<td>" . $receta->getIngrediente() . "</td>";
+					echo "<td>" . $receta->getPreparacion() . "</td>";
+					echo "<td>" . $receta->getTiempo() . "</td>";
+					echo "<tr>";
+				}
+	
+			?>
+		  
+		</table>
+		
+		<?php
+	}
+	
 }
+
+	?>
+	
