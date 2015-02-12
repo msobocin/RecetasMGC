777d26c5327811a826d7848c620393a1c21ad13f
Michal Sobocinski <msobocin@jaumebalmes.net>
2015-02-13 00:01:21 +0100

    ejemplo de insertar datos

--- index.php
+++ index.php
@@ -10,16 +10,16 @@
 // require_once 'Model/Ingrediente.php';
 
 // $ingrediente = new Ingrediente();
-// $ingrediente2 = new Ingrediente();
+// // $ingrediente2 = new Ingrediente();
 // $receta = new Receta();
 
 // $ingrediente->setAll(1,"Azucar","kg/kgs","3/4");
-// $ingrediente2->setAll(1,"Azucar","kg/kgs","3/4");
+
 // $arrIngredientes = array();
-// array_push($arrIngredientes, $ingrediente);
-// array_push($arrIngredientes, $ingrediente2);
-// $ingrediente2->setAll(2,"Vodka","litro/s","5");
-// array_push($arrIngredientes, $ingrediente2);
+// array_push($arrIngredientes,clone $ingrediente);
+
+// $ingrediente->setAll(2,"Vodka","litro/s","5");
+// array_push($arrIngredientes,clone $ingrediente);
 
 // $receta->setAll(0,"Vodka con Azucar",$arrIngredientes,"Mesclar azucar con vodka!",0,2);
 
