463d5531a4be09e8311050b6ceb312bb15dede94
Michal Sobocinski <msobocin@jaumebalmes.net>
2015-02-14 13:48:27 +0100

    pie chart example

--- index.php
+++ index.php
@@ -24,22 +24,69 @@ require_once 'Controler/IngredienteControler.php';
 
 // $receta->setAll(0,"Vodka con Azucar",$arrIngredientes,"Mesclar azucar con vodka!",0,2);
 
-$recetaBD = new ReceptaControler();
+// $recetaBD = new ReceptaControler();
 // $recetaBD->save($receta);
-$arrRecetas=$recetaBD->consultRecetaId(1);
+// $arrRecetas=$recetaBD->consultRecetaId(1);
 // $recetaBD->consult();
 
 // foreach ($arrRecetas as $receta) {
-	echo $arrRecetas->getNombre()."<br/>";
-	foreach ($arrRecetas->getIngredientes() as $ingrediente) {
-		echo $ingrediente->getIngrediente()."<br/>";
-	}
+// 	echo $arrRecetas->getNombre()."<br/>";
+// 	foreach ($arrRecetas->getIngredientes() as $ingrediente) {
+// 		echo $ingrediente->getIngrediente()."<br/>";
+// 	}
 // }
 
 $ingrediente=new IngredienteControler();
 
-echo $ingrediente->consultCuantas(1);
+// echo $ingrediente->consultCuantas(1);
 
 // echo $ingrediente->consultCuantas(1);
+
+?>
+<script type="text/javascript" src="http://www.amcharts.com/lib/3/amcharts.js"></script>
+<script type="text/javascript" src="http://www.amcharts.com/lib/3/pie.js"></script>
+<script type="text/javascript" src="http://www.amcharts.com/lib/3/themes/chalk.js"></script>
+
+<style>
+@import url(http://fonts.googleapis.com/css?family=Covered+By+Your+Grace);
+#chartdiv {	
+	width		: 100%;
+	height		: 500px;
+	font-size	: 11px;
+}
+</style>
+
+<div id="chartdiv"></div>	
+
+<?php 
+echo "<script>";
+echo 'var chart = AmCharts.makeChart("chartdiv", {
+    "type": "pie",	
+	"theme": "none",
+    "legend": {
+        "markerType": "circle",
+        "position": "right",
+		"marginRight": 80,		
+		"autoMargins": false
+    },
+    "dataProvider": [{
+        "cuantos": "Leche",
+        "ingrediente": '.$ingrediente->consultCuantas(1).'
+    }, {
+        "cuantos": "Arroz",
+        "ingrediente": '.$ingrediente->consultCuantas(2).'
+    }],
+    "valueField": "ingrediente",
+    "titleField": "cuantos",
+    "balloonText": "[[title]]<br><span style=\'font-size:14px\'><b>[[value]]</b> ([[percents]]%)</span>",
+    "exportConfig": {
+        "menuTop":"0px",
+        "menuItems": [{
+            "icon": \'/lib/3/images/export.png\',
+            "format": \'png\'
+        }]
+    }
+});';
+echo "</script>";
 
 ?>
